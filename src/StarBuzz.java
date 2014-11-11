0ca68665baec2dcddce31b88733085ff064669bd
Barnaby Colby <barnaby@linux-ptxh.site>
2014-11-11 22:56:43 +0000

    Added house blend beverage

--- src/StarBuzz.java
+++ src/StarBuzz.java
@@ -2,8 +2,11 @@ public class StarBuzz {
 	public static void main(String[] args) {
 		// Create an espresso object and print it's cost
 		Beverage espresso = new Espresso();
-
 		System.out.println("An " + espresso.getDescription() + " costs $" + espresso.cost());
+
+		// Create a house blend object and print it's cost
+		Beverage houseBlend = new HouseBlend();
+		System.out.println("A " + houseBlend.getDescription() + " costs $" + houseBlend.cost());
 
 		return;
 	}
